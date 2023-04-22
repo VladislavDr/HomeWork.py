6c3750bdb54bfa8d493ee204a3da70bf895c4252
Vladislad Dr <despod96@mail.ru>
2023-04-22 22:48:27 +0300

    HomeWork 5

--- HomeWork5/task2.py
+++ HomeWork5/task2.py
@@ -5,37 +5,28 @@
 # более 1 раза, к нему добавляется количество повторений.
 
 def count_let(x):
-    some_list = []
     count = 1
     temp = []
+    first_symbol = True
     for i in range(0, len(x)-1):
         if x[i] == x[i + 1]:
+            if first_symbol:
+                temp.append(x[i])
+                first_symbol = False
             count += 1
         else:
-            if count == 1:
-                temp.append(x[i])
-            else:
-                temp.append(x[i])
-                temp.append(count)
-            some_list.append(temp)
-            temp = []
+            if count != 1:
+                temp.append(str(count))
             count = 1
-    # if x[-1] == x[-2]:
-    #     temp.append(x[-1])
-    #     temp.append(count)
-    #     some_list.append(temp)
-    # elif x[-1] != x[-2]:
-    #     temp.append(x[-1])
-    #     temp.append(count)
-    #     some_list.append(temp)
-    print_list = []
-    for i in some_list:
-        if len(i) > 1:
-            print_list.append(f"{i[0]}{i[-1]}")
-        else:
-            print_list.append(i[0])
-    print(*print_list, sep= '')
+            first_symbol = True
+        if i == len(x)-2 and first_symbol:
+            temp.append(x[i+1])
+    if count != 1:
+        temp.append(str(count))
+    print(temp)
+    x = "".join(temp)
+    print(x)
 
-x = str(input("Введите буквы в цельную строку ")) + " "
+x = str(input("Введите буквы в цельную строку "))
 lst = (x)
 count_let(lst)
